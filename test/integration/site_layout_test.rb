87308a207b69c0a0d1836ae4e44507e78ec2f3f9
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-06-13 08:52:26 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,54 @@
 require "test_helper"
 
-class SiteLayoutTest < ActionDispatch::IntegrationTest
+class SiteLayout < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+end
+
+class SiteLayoutTest < SiteLayout
+
+  test "layout links before login" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    get help_path
+    assert_select "title", full_title("Help")
+    get about_path
+    assert_select "title", full_title("About")
+    get login_path
+    assert_select "title", full_title("Log in")
+  end
+
+  test "layout links after login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get user_path(@user)
+    assert_select "title", full_title(@user.name)
+    get help_path
+    assert_select "title", full_title("Help")
+    get about_path
+    assert_select "title", full_title("About")
+    get edit_user_path(@user)
+    assert_select "title", full_title("Edit user")
   end
 end
