03b14d75ebc0e1991601be4785296368e5e265fd
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-06-22 03:15:44 +0000

    Add user microposts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+
+    # ユーザーのログインを確認する
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Plese log in"
+        redirect_to login_url, status: :see_other
+      end
+    end
 end
